07a9db8b5d4345d6bb5dd5d2be729411cbce32a1
Daniel DiPietro Jr. <75383640+dcdjr@users.noreply.github.com>
2026-05-08 16:59:11 -0400

    Update heartbeat documentation

--- include/sentinel/agent/heartbeat.hpp
+++ include/sentinel/agent/heartbeat.hpp
@@ -1,8 +1,6 @@
 #pragma once
 
-/* Heartbeat is meant to send system info and other important metadata to
-   collector The heartbeat is sent every N seconds, is sent in structured JSON
-   format, and will eventually send over TCP but for now it just sends to
-   stdout. */
+/* Heartbeat sends periodic host status and metadata to the collector.
+   The heartbeat is sent every N seconds in structured JSON format. */
 
 void send_heartbeat();
